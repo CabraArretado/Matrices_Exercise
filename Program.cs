fe86539429b7ac1ca9ab6016df6c8c54000f9e3a
Felipe Moura <42254622+felipetempus@users.noreply.github.com>
2021-01-12 12:53:32 -0600

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,28 @@
+﻿using System;
+
+namespace Matrices_Exercise
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Square matrix size: ");
+            var size = int.Parse(Console.ReadLine());
+
+            int[,] matrix = new int[size, size];
+
+            for(int i = 0; i < size; i++)
+            {
+                string[] line = Console.ReadLine().Split(" ");
+                
+                for(int ii = 0; ii < size; ii++)
+                {
+                    matrix[i, ii] = int.Parse(line[ii]);
+                }
+                
+            }
+
+
+        }
+    }
+}
